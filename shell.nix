beab8e933413a145e19e627f29ce64264e5d67af
Ismail Mailanyi <me.ismailanyi@gmail.com>
2026-02-06 07:40:31 +0300

    feat(com): Initial shell.nix for android development

--- shell.nix
+++ shell.nix
@@ -1,12 +1,37 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { config = { 
+  allowUnfree = true;
+   }; } }:
 
-pkgs.mkShell {
-  packages = [
-    pkgs.nodejs_22  # Let's stick to the current LTS to be safe
-    pkgs.postgresql
-  ];
+let 
+  androidSdk = pkgs.androidenv.composeAndroidPackages {
+    cmdLineToolsVersion = "13.0";
+    # toolsVersion ? "latest",
+    platformToolsVersion = "35.0.1";
+    buildToolsVersions = [ "34.0.0" ];
+    platformVersions = [ "34" ];
 
-  shellHook = ''
-    echo "Environment Loaded: Node $(node --version) | Postgres $(postgres --version)"
-  '';
-}
+    includeEmulator = false;
+    includeSystemImages = false;
+  };
+
+in
+  pkgs.mkShell {
+    packages = with pkgs; [
+      nodejs_24
+      postgresql
+      jdk17
+      androidSdk.androidsdk
+    ];
+
+    ANDROID_HOME = "${androidSdk.androidsdk}/libexec/android-sdk";
+
+    shellHook = ''
+      echo "Environment Loaded!"
+      echo "Node: $(node --version)"
+      echo "Java: $(java -version 2>&1 | head -n 1)"
+      echo "Environment Loaded: Node $(node --version) | Postgres $(postgres --version)"
+      echo "Android SKD location: $ANDROID_HOME"
+      export PGHOST=$PWD
+      echo $PGHOST
+    '';
+  }
