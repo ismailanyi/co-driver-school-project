9783f653377c80121b5b5182e6a29226d398cab3
Ismail Mailanyi <me.ismailanyi@gmail.com>
2026-02-11 14:12:04 +0300

    chore: no longer handles pool and register

--- co-driver-backend/index.js
+++ co-driver-backend/index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const { Pool } = require('pg');
-const bcrypt = require('bcrypt');
 require('dotenv').config();
 
 const app = express();
@@ -9,41 +7,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const pool = new Pool({
-    user: process.env.PGUSER,
-    host: process.env.PGHOST,
-    database: process.env.PGDATABASE,
-    password: process.env.PGPASSWORD,
-    port: process.env.PGPORT,
-})
-
 app.get('/', (req,res) => {
     res.json({
         message: 'Welcome to the Co-Driver Backend API'
     })
 })
 
-app.post('/register', async(req, res) => {
-    try {
-        const { phone_number, email, password, first_name, last_name, provider, provider_id } = req.body;
-        const password_hash = await bcrypt.hash(req.body.password, 10)
 
-        const newUser = await pool.query(
-            'INSERT INTO users (phone_number, email, password_hash, first_name, last_name, provider, provider_id) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *', 
-            [phone_number, email, password_hash, first_name, last_name, provider, provider_id]
-        )
-        console.log('Saving user:', {email, phone_number})
-        
-        
-        res.json({
-            message: 'User registered successfully',
-            user: {phone_number, email},
-        })
-    } catch (err) {
-
-        req.status(500).send("Server Error");
-    }
-})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
