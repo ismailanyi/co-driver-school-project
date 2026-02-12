3ac2ba3da93486320d377e2d3a3c19366c9dafd6
Ismail Mailanyi <me.ismailanyi@gmail.com>
2026-02-12 15:58:08 +0300

    feat: use JwtAuth

--- co-driver-backend/index.js
+++ co-driver-backend/index.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/auth', require('./routes/jwtAuth'))
+
 app.get('/', (req,res) => {
     res.json({
         message: 'Welcome to the Co-Driver Backend API'
