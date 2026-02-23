bbe684937a5a292cf52d2277a114b5147c41a17c
Ismail Mailanyi <me.ismailanyi@gmail.com>
2026-02-23 20:25:45 +0300

    feat: use phone instead of phone_number
    - use destructured variable

--- backend/routes/jwtAuth.js
+++ backend/routes/jwtAuth.js
@@ -5,19 +5,20 @@ const pool = require('../db');
 
 router.post('/signup', async(req, res) => {
     try {
-        const { phone_number, email, password, first_name, last_name, provider, provider_id } = req.body;
-        const password_hash = await bcrypt.hash(req.body.password, 10)
+        const { phone, email, password, first_name, last_name, provider, provider_id } = req.body;
+        const password_hash = await bcrypt.hash(password, 10)
         
         const newUser = await pool.query(
-            'INSERT INTO users (phone_number, email, password_hash, first_name, last_name, provider, provider_id) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *', 
-            [phone_number, email, password_hash, first_name, last_name, provider, provider_id]
+            'INSERT INTO users (phone, email, password_hash, first_name, last_name, provider, provider_id) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *', 
+            [phone, email, password_hash, first_name, last_name, provider, provider_id]
         )
-        console.log('Saving user:', {email, phone_number})
+
+        console.log('Saving user:', {email, phone})
         
         
         res.json({
             message: 'User registered successfully',
-            user: {phone_number, email},
+            user: {phone, email},
         })
     } catch (err) {
         console.error(err);
