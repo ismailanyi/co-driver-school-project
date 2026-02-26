0ba6c31c8c4e3f7067cd2f2715f4ce24a7781262
Ismail Mailanyi <me.ismailanyi@gmail.com>
2026-02-26 22:10:32 +0300

    feat: use new assets

--- backend/index.js
+++ backend/index.js
@@ -6,17 +6,14 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use(express.static('public'))
+app.use('/assets', express.static(path.join(__dirname, '../assets')))
 app.use('/auth', require('./routes/jwtAuth'))
-
 app.get('/', (req,res) => {
     res.json({
         message: 'Welcome to the Co-Driver Backend API'
     })
 })
-
-
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`)
