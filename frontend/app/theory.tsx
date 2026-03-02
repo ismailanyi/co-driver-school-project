9401dd20724307c31681aaed669539464d70f8ea
Ismail Mailanyi <me.ismailanyi@gmail.com>
2026-03-02 12:23:23 +0300

    feat: Iniital Theory page

---
+++ frontend/app/theory.tsx
@@ -0,0 +1,17 @@
+import {  TouchableOpacity, StyleSheet } from "react-native";
+import { ThemedView } from "@/components/themed-view";
+import { ThemedText } from "@/components/themed-text";
+
+const theory = () => {
+
+    return (
+        <ThemedView>
+            <TouchableOpacity>
+
+            </TouchableOpacity>
+        </ThemedView>
+    )
+
+}
+
+export default theory;
