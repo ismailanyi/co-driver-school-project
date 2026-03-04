3ed587f7d4dba2ce819a5558ffa7e34486e290ff
Ismail Mailanyi <me.ismailanyi@gmail.com>
2026-03-04 22:33:42 +0300

    feat: minor ui fixes

--- frontend/app/signin.tsx
+++ frontend/app/signin.tsx
@@ -1,100 +1,94 @@
-import { useState } from 'react';
-import { StyleSheet, Platform } from 'react-native';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { ThemedTextInput } from '@/components/ThemedTextInput';
-import { ThemedButton } from '@/components/themed-button';
-import * as SecureStore from 'expo-secure-store'; 
-import { router } from 'expo-router';
-import { globalStyles } from '@/constants/globalStyles';
+import { ThemedButton } from "@/components/themed-button";
+import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
+import { ThemedTextInput } from "@/components/ThemedTextInput";
+import { globalStyles } from "@/constants/globalStyles";
+import axios from "axios";
+import { router } from "expo-router";
+import * as SecureStore from "expo-secure-store";
+import { useState } from "react";
+import { Platform } from "react-native";
 
 const SignInScreen = () => {
   const [formData, setFormData] = useState({
-      identifier: '',
-      password: '',
+    identifier: "",
+    password: "",
   });
-  const [errorMessage, setErrorMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState("");
 
   const updateField = (key: string, value: string) => {
-    setFormData((prev)=> ({...prev, [key]: value}) )
-    setErrorMessage('');
+    setFormData((prev) => ({ ...prev, [key]: value }));
+    setErrorMessage("");
   };
 
   const handleSignIn = async () => {
-    setErrorMessage('');
+    setErrorMessage("");
     try {
-      console.log('Here is the expo link: ', process.env.EXPO_PUBLIC_API_URL)
-      const response = await fetch (`${process.env.EXPO_PUBLIC_API_URL}/auth/signin`,{
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData)
-      })
+      console.log("Here is the expo link: ", process.env.EXPO_PUBLIC_API_URL);
+      const api = axios.create({
+        baseURL: process.env.EXPO_PUBLIC_API_URL,
+      });
+      const response = await api.post("/auth/signin");
 
-      const { message, token } = await response.json();
-      if (!response.ok) {
-        console.error('Login failed: ', message)
-        setErrorMessage('Invalid username/ password')
-        return;
-      }
+      const { message, token } = await response.data;
 
-      if (Platform.OS === 'web') {
-        localStorage.setItem('userToken', token)
+      if (Platform.OS === "web") {
+        localStorage.setItem("userToken", token);
       } else {
-        await SecureStore.setItemAsync('userToken', token)
+        await SecureStore.setItemAsync("userToken", token);
       }
-      
-      console.log('Success: ', message);
-      router.replace('/home')
-      
 
+      console.log("Success: ", message);
+      router.replace("/home");
     } catch (error) {
-      console.error('Error: Failed ', error)
-      
+      console.error("Error: Failed ", error);
     }
-  }
+  };
   // Theme
-    
+
   return (
     <ThemedView style={globalStyles.container}>
-      <ThemedText type="title" style={{marginBottom: 20, fontSize: 20}}>Enter your details</ThemedText>
+      <ThemedText type="title" style={{ marginBottom: 20, fontSize: 20 }}>
+        Enter your details
+      </ThemedText>
       {errorMessage ? (
-        <ThemedText style={{color: 'red', marginBottom: 10}}>
+        <ThemedText style={{ color: "red", marginBottom: 10 }}>
           {errorMessage}
         </ThemedText>
-      ): null}
-        <ThemedView style={{ gap: 0, marginBottom: 10}}>
-          <ThemedTextInput
-            placeholder='Email, Phone or username'
-            value={formData.identifier}
-            keyboardType='email-address'
-            autoCapitalize='none'
-            onChangeText={(text) => updateField('identifier', text)}
-          />
-          <ThemedTextInput
-            placeholder='password'
-            value={formData.password}
-            autoCapitalize='none'
-            onChangeText={(text) => updateField('password', text)}
-            secureTextEntry
-          />
-        </ThemedView>
-        <ThemedView>
-          <ThemedButton
-            title = "Sign In"
-            onPress={() => handleSignIn()}
-            disabled={!formData.identifier || !formData.password}
-          />
-          <ThemedButton
-            title='FORGOT PASSWORD'
-            style={globalStyles.forgotPasswordContainer}
-            textStyle={globalStyles.forgotPasswordText}
-            onPress={() => {router.push('/forgot')}}
-          />
-        </ThemedView>
+      ) : null}
+      <ThemedView style={{ gap: 0, marginBottom: 10 }}>
+        <ThemedTextInput
+          placeholder="Email, Phone or username"
+          value={formData.identifier}
+          keyboardType="email-address"
+          autoCapitalize="none"
+          onChangeText={(text) => updateField("identifier", text)}
+        />
+        <ThemedTextInput
+          placeholder="password"
+          value={formData.password}
+          autoCapitalize="none"
+          onChangeText={(text) => updateField("password", text)}
+          secureTextEntry
+        />
+      </ThemedView>
+      <ThemedView>
+        <ThemedButton
+          title="Sign In"
+          onPress={() => handleSignIn()}
+          disabled={!formData.identifier || !formData.password}
+        />
+        <ThemedButton
+          title="FORGOT PASSWORD"
+          style={globalStyles.forgotPasswordContainer}
+          textStyle={globalStyles.forgotPasswordText}
+          onPress={() => {
+            router.push("/forgot");
+          }}
+        />
+      </ThemedView>
     </ThemedView>
-  )
-}
+  );
+};
 
 export default SignInScreen;
