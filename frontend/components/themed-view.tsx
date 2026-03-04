a5eaebcba47132450811f286d5df295275d89cad
Ismail Mailanyi <me.ismailanyi@gmail.com>
2026-03-04 18:36:29 +0300

    feat: ThemedBox use children

--- frontend/components/themed-view.tsx
+++ frontend/components/themed-view.tsx
@@ -1,14 +1,24 @@
 import { View, type ViewProps } from 'react-native';
-
 import { useThemeColor } from '@/hooks/use-theme-color';
+import { globalStyles } from '@/constants/globalStyles';
+import { PropsWithChildren } from 'react';
 
-export type ThemedViewProps = ViewProps & {
+export type ThemedViewProps = ViewProps & PropsWithChildren<{
   lightColor?: string;
   darkColor?: string;
-};
+}>;
 
-export const ThemedView = ({ style, lightColor, darkColor, ...otherProps }: ThemedViewProps) => {
+export const ThemedView = ({ 
+  style, 
+  lightColor, 
+  darkColor, 
+  children,
+  ...otherProps }: ThemedViewProps) => {
   const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
 
-  return <View style={[{ backgroundColor }, style]} {...otherProps} />;
+  return (
+    <View style={[globalStyles.container, { backgroundColor }, style]} {...otherProps}>
+      {children}
+    </View>
+    );
 }
