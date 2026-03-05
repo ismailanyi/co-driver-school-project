85a28ad92942417528d1d2baed2469c369ddd1a3
Ismail Mailanyi <me.ismailanyi@gmail.com>
2026-03-05 03:15:29 +0300

    feat: new submit answer component

---
+++ frontend/components/submit-answer.tsx
@@ -0,0 +1,32 @@
+import { ThemedView } from "./themed-view";
+import { ThemedText } from "./themed-text";
+import { TouchableOpacity } from "react-native";
+import { globalStyles } from "@/constants/globalStyles";
+import { useState } from "react";
+
+interface submitAnswerProps {
+    selectedId: number | null;
+    isSubmitted: boolean;
+    isCorrect: boolean | null;
+    onPress: () => void;
+}
+
+const SubmitAnswer = ({selectedId, onPress, isSubmitted, isCorrect }: submitAnswerProps) => {
+    return (
+        <ThemedView>
+            <TouchableOpacity
+                style={[globalStyles.submitButton, !selectedId ? globalStyles.disabledButton : isSubmitted && !isCorrect && globalStyles.incorrectAnswer]}
+                disabled={!selectedId}
+                onPress={onPress}
+            >
+                <ThemedText style={[globalStyles.submitText]}
+                >
+                    {isSubmitted ? isCorrect ? 'CONTINUE' : 'GOT IT' : 'CHECK'} 
+                </ThemedText>
+            </TouchableOpacity>
+        </ThemedView>
+        
+    );
+};
+
+export default SubmitAnswer;
