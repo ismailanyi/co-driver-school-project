50f17ec84fd697ea082e9a2b2e89768e4f74584d
Ismail Mailanyi <me.ismailanyi@gmail.com>
2026-03-05 03:09:25 +0300

    feat: no longer handle stylesheet

--- frontend/app/forgot.tsx
+++ frontend/app/forgot.tsx
@@ -1,4 +1,3 @@
-import { StyleSheet } from "react-native";
 import { ThemedView } from "@/components/themed-view";
 import { ThemedText } from "@/components/themed-text";
 import { ThemedTextInput } from "@/components/ThemedTextInput";
