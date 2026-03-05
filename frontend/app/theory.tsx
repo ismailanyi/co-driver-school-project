3504d3f73932483e7f97fe050bc765e0d0d567f7
Ismail Mailanyi <me.ismailanyi@gmail.com>
2026-03-05 03:18:02 +0300

    feat: new theory page

--- frontend/app/theory.tsx
+++ frontend/app/theory.tsx
@@ -1,17 +1,11 @@
-import {  TouchableOpacity, StyleSheet } from "react-native";
+import { TouchableOpacity, StyleSheet } from "react-native";
 import { ThemedView } from "@/components/themed-view";
 import { ThemedText } from "@/components/themed-text";
+import Questions from "@/components/questions";
+import SubmitAnswer from "@/components/submit-answer";
+import axios from "axios";
 
-const theory = () => {
+export const theory = async () => {
+  const quesions = await axios.get("");
+};
 
-    return (
-        <ThemedView>
-            <TouchableOpacity>
-
-            </TouchableOpacity>
-        </ThemedView>
-    )
-
-}
-
-export default theory;
