800df003c41b7f8cfecd0e72fb90c7111fc5a4ff
Ismail Mailanyi <me.ismailanyi@gmail.com>
2026-03-05 03:13:13 +0300

    feat: route quiz

--- backend/index.js
+++ backend/index.js
@@ -10,12 +10,13 @@ app.use(express.json());
 app.use(express.static('public'))
 app.use('/assets', express.static(path.join(__dirname, '../assets')))
 app.use('/auth', require('./routes/jwtAuth'))
-app.use('/signs', require('./routes/quiz'))
+app.use('/quiz', require('./routes/quiz'))
 app.get('/', (req,res) => {
     res.json({
         message: 'Welcome to the Co-Driver Backend API'
     })
 })
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`)
