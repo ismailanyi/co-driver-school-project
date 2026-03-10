a52dc934c1e43da74ef767b4403df7498271f475
Ismail Mailanyi <me.ismailanyi@gmail.com>
2026-03-10 13:12:29 +0300

    feat: send theory endpoint

--- frontend/app/theory.tsx
+++ frontend/app/theory.tsx
@@ -1,7 +1,7 @@
 import { useQuestions } from "@/hooks/use-questions";
 
 const Theory = () => {
-  const {isCorrect,questions,targetQuestion,isSubmitted,selectedId,setSelectedId,} = useQuestions();
+  const {isCorrect,questions,targetQuestion,isSubmitted,selectedId,setSelectedId,} = useQuestions('theory');
 };
 
 export default Theory;
