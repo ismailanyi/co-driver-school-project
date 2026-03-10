ef7d5640616b2a20484fd2fdef48def8e86e4c77
Ismail Mailanyi <me.ismailanyi@gmail.com>
2026-03-10 13:09:39 +0300

    feat: use hook and component

--- frontend/app/theory.tsx
+++ frontend/app/theory.tsx
@@ -1,11 +1,7 @@
-import { TouchableOpacity, StyleSheet } from "react-native";
-import { ThemedView } from "@/components/themed-view";
-import { ThemedText } from "@/components/themed-text";
-import Questions from "@/components/questions";
-import SubmitAnswer from "@/components/submit-answer";
-import axios from "axios";
+import { useQuestions } from "@/hooks/use-questions";
 
-export const theory = async () => {
-  const quesions = await axios.get("");
+const Theory = () => {
+  const {isCorrect,questions,targetQuestion,isSubmitted,selectedId,setSelectedId,} = useQuestions();
 };
 
+export default Theory;
