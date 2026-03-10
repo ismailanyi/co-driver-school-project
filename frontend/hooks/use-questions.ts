c371ea6d1b14b3a62071fbcf26ccd799edd74a86
Ismail Mailanyi <me.ismailanyi@gmail.com>
2026-03-10 13:09:20 +0300

    feat: new quesions hook

---
+++ frontend/hooks/use-questions.ts
@@ -0,0 +1,56 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+interface Question {
+  id: number;
+  category?: string;
+  question: string,
+}
+
+export const useQuestions =  (endpoint: string) => {
+    const [selectedId, setSelectedId] = useState<number | null>(null);
+    const [questions, setQuestion ] = useState([]);
+    const [targetQuestion, setTargetQuestion] = useState<Question | null>(null)
+    const [isCorrect, setIsCorrect] = useState<boolean | null>(false);
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    
+    const fetchQuestion = async (endpoint: string) => {
+        try {
+            const api = axios.create({
+                baseURL: process.env.EXPO_PUBLIC_API_URL
+            })
+            const response = await api.get(`/quiz/${endpoint}`)
+            const fetchedSigns = response.data;
+            const randomTarget = fetchedSigns[Math.floor(Math.random() * fetchedSigns.length)];
+            setTargetQuestion(randomTarget);
+            setQuestion(fetchedSigns)
+        } catch (error) {
+            console.error("Error failed to get signs: ", error);
+        }
+    }
+    
+    useEffect(() => {
+        fetchQuestion(endpoint);
+    }, [])
+
+    const handleSubmit = () => {
+        if (!isSubmitted) {
+            setIsCorrect(targetQuestion && selectedId === targetQuestion.id);
+            setIsSubmitted(true)
+        } else {
+            setIsSubmitted(false)
+            setSelectedId(null)
+            fetchQuestion(endpoint);
+        }
+    }
+
+    return {
+        questions,
+        targetQuestion,
+        isCorrect,
+        isSubmitted,
+        selectedId,
+        setSelectedId,
+        fetchQuestion
+    }
+}
