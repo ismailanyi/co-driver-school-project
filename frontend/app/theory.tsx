77ecd235c21f819a31e90c0be5efe99443865317
Ismail Mailanyi <me.ismailanyi@gmail.com>
2026-03-11 11:33:30 +0300

    chore: remove unnecessary files

--- frontend/app/theory.tsx
+++ frontend/app/theory.tsx
@@ -1,8 +1,6 @@
-import { useQuestions } from "@/hooks/use-questions";
 import QuestionScreen from "@/components/question-screen";
 
 const Theory = () => {
-  const {isCorrect,questions,targetQuestion,isSubmitted,selectedId,setSelectedId,} = useQuestions('theory');
   return (
     <QuestionScreen
       question_type="theory"
