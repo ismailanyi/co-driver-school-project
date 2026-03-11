38ce8c7b3c323048d5099a8940b56e569bdcf060
Ismail Mailanyi <me.ismailanyi@gmail.com>
2026-03-11 11:38:30 +0300

    chore: rename sytles

--- frontend/constants/globalStyles.ts
+++ frontend/constants/globalStyles.ts
@@ -1,3 +1,4 @@
+import QuestionScreen from "@/components/question-screen";
 import { StyleSheet } from "react-native";
 
 export const globalStyles = StyleSheet.create({
@@ -11,9 +12,6 @@ export const globalStyles = StyleSheet.create({
         color: 'green',
         alignItems: 'center'
     },
-    normal: {
-        color: 'gray'
-    },
     textcontainer: {
         flex: 1,
         alignItems: 'center',
@@ -23,10 +21,19 @@ export const globalStyles = StyleSheet.create({
         color: 'red',
     },
     container: {
+        position: "relative",
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
         padding: 20,
+    },
+    homeButtonsStyles: {
+        flex: 1,
+        backgroundColor: '#EFBF04'
+    },
+    homeTextStyle: {
+        fontSize: 20,
+        color: '#000000'
     },
     link: {
         marginTop: 15,
@@ -36,36 +43,10 @@ export const globalStyles = StyleSheet.create({
             color: '#FFFFFF',
             fontSize: 16,
     },
-    inputtext: {
-        color: '#1e1f20'
-    },
     titleContainer: {
         flexDirection: 'row',
         alignItems: 'center',
         gap: 8,
-    },
-    stepContainer: {
-        gap: 8,
-        marginBottom: 8,
-    },
-    reactLogo: {
-        height: 178,
-        width: 290,
-        bottom: 0,
-        left: 0,
-        position: 'absolute',
-    },
-    input: {
-        height: 40,
-        borderWidth: 1,
-        borderRadius: 4,
-        padding: 8,
-    },
-    forgotPasswordText: {
-        color: '#00BFFF',
-        fontSize: 14,
-        fontWeight: '500',
-        textAlign: 'center'
     },
     forgotPasswordContainer: {
         backgroundColor: 'transparent',
@@ -73,8 +54,22 @@ export const globalStyles = StyleSheet.create({
         shadowOpacity: 0,
         marginTop: 10,
     },
-    checker: {
-        alignSelf: 'stretch'
+    forgotPasswordText: {
+        color: '#00BFFF',
+        fontSize: 14,
+        fontWeight: '500',
+        textAlign: 'center'
+    },
+    questionScreenContainer: {
+        flex: 1,
+        justifyContent: 'space-between',
+        padding: 20,
+        paddingBottom: 20,
+        // alignItems: "flex-start",
+    },
+    questionContainer: {
+        width: '100%',
+        flex: 6
     },
     promptText: {
         fontSize: 20,
@@ -87,13 +82,6 @@ export const globalStyles = StyleSheet.create({
         marginBottom: 30,
         textAlign: 'center'
     },
-    img: {
-        width: '20%',
-        aspectRatio: 1,
-        backgroundColor: '#e5e5e5',
-        borderRadius: 15,
-        marginBottom: 15,
-    },
     grid: {
         flexDirection: 'row',
         flexWrap: 'wrap',
@@ -101,9 +89,8 @@ export const globalStyles = StyleSheet.create({
         gap: 12,
         padding: 0,
         width: '100%'
-
     },
-    card: {
+    signsCard: {
         width: '40%',
         aspectRatio: 1,
         borderWidth: 2,
@@ -113,40 +100,45 @@ export const globalStyles = StyleSheet.create({
         marginBottom: 15,
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: '#ffffff'
     },
     selectedCard: {
         borderColor: '#84D8FF',
         backgroundColor: '#DDF4FF'
     },
-    signImage: {
+    signImageSize: {
         height: '80%',
         width: '80%',
     },
+    submitContainer: {
+        paddingTop: 10,
+        width: '100%',
+        height: '30%',
+        marginBottom: 0,
+        justifyContent: "flex-end",
+        flex: 1
+    },
     submitButton: {
         width: '100%',
-        backgroundColor: '#58CC02',
+        backgroundColor: '#EFBF04',
         padding: 15,
         borderRadius: 15,
         alignItems: 'center',
-        marginTop: 20,
     },
-    disabledButton: {
-        // color: 'white',
-        // fontWeight: 'bold',
-        backgroundColor: '#e5e5e5'
-    },
-    submitText: {
+    submitButtonText: {
         fontWeight: 'bold',
         fontSize: 18,
         letterSpacing: 1,
     },
-    incorrectAnswer: {
-        borderRadius: 1,
+    correctAnswerButton: {
+        borderRadius: 15,
+        backgroundColor: '#58CC02'
+    },
+    incorrectAnswerButton: {
+        borderRadius: 15,
         borderColor: '#ff0000',
         backgroundColor: '#ff0000'
     }, 
-    theoryButton: {
+    theoryOptionCard: {
         width: '100%',
         borderWidth: 2,
         borderColor: '#e5e5e5',
@@ -155,17 +147,12 @@ export const globalStyles = StyleSheet.create({
         marginBottom: 10,
         backgroundColor: '#ffffff',
     },
-    theoryText: {
+    theoryOptionText: {
         fontSize: 16,
         color: '#4b4b4b',
     },
-    bottomContainer: {
-        width: '100%',
-        paddingTop: 10,
-        marginTop: 'auto',
-    },
     button: {
-        backgroundColor: '#58CC02',
+        backgroundColor: '#EFBF04',
         padding: 15,
         borderRadius: 15,
         alignItems: 'center',
@@ -181,7 +168,7 @@ export const globalStyles = StyleSheet.create({
         shadowOpacity: 0.25,
         shadowRadius: 4
     },
-    disabled: {
+    disabledButton: {
         backgroundColor: "#e5e5e5",
         elevation: 0,
         shadowOpacity: 0,
