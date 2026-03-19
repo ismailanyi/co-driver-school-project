3b60f26ddde48089e13ee5bc579dc21abb5d4b0f
Ismail Mailanyi <me.ismailanyi@gmail.com>
2026-03-19 11:13:38 +0300

    feat: mtb boilerplate

--- frontend/app/mtb.tsx
+++ frontend/app/mtb.tsx
@@ -1,5 +1,28 @@
+import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
+import { globalStyles } from "@/constants/globalStyles";
+import { Image } from "expo-image";
+
 const mtb = () => {
+  return (
+    <ThemedView style={globalStyles.container}>
+        <ThemedText>
+            This screen will have The Model Town Board 2D scenarios. 
+        </ThemedText>
+        <ThemedText>
+            Coming soon
+        </ThemedText>
+        <ThemedText>
+             ---
+        </ThemedText>
 
-}
+        <Image
+            source={require("../assets/MTB2.png")}
+            contentFit="contain"
+            style={{ width: "100%", height: 400 }}
+        />
+    </ThemedView>
+  );
+};
 
-export default mtb
+export default mtb;
